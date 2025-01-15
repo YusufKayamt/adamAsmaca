29815b4c887c393950e28a3988e38819cbcaeaa9
Yusuf Mete Kaya <kayayusufmete@gmail.com>
2025-01-15 21:57:54 +0300

    adamAsmaca
    developed with c#

---
+++ adamAsmaca/Form1adamAsmaca.Designer.cs
@@ -0,0 +1,56 @@
+﻿namespace WindowsFormsApp4
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // Form1
+            // 
+            this.ClientSize = new System.Drawing.Size(938, 450);
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private DevExpress.XtraEditors.TextEdit textEdit2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ adamAsmaca/Form1adamAsmaca.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp4
+{
+    public partial class Form1 : Form
+    {
+        
+        String[] kelimeler = { "ali" };
+        private int no;
+        private char[] gizliKelime;
+        private string secilenKelime;
+        private string dogruKelime;
+        private string tahminKelime;
+        private char karakterler;
+        private string charİcin;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rastgelesayi = new Random();
+            no = rastgelesayi.Next(0, kelimeler.Length);
+            dogruKelime = kelimeler[no];
+            secilenKelime = kelimeler[no];
+            charİcin = secilenKelime;
+            gizliKelime = new char[secilenKelime.Length];
+            for (int i = 0; i < gizliKelime.Length; i++)
+            {
+                gizliKelime[i] = '*';  // kelimeyi gizli olarak yazma
+          
+                textBox1.Text = (secilenKelime);
+            }
+
+            
+
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            // harf alma
+            string girilenHarf = textEdit2.Text.Trim();
+
+            // tek harf girme
+            if (string.IsNullOrEmpty(girilenHarf) || girilenHarf.Length != 1)
+            {
+                MessageBox.Show("Lütfen sadece bir harf girin.");
+                return;
+            }
+
+            bool harfBulundu = false;  // harf var mı yokmu kontrolü
+
+            if (secilenKelime.Contains(girilenHarf))
+            {
+                for (int i = 0; i < secilenKelime.Length; i++)
+                {
+                    // harf varsa kelime güncelleme
+                    if (secilenKelime[i] == girilenHarf[0])
+                    {
+                        gizliKelime[i] = girilenHarf[0];  
+                        harfBulundu = true;  
+                    }
+                }
+            }
+            // harfi ekrana yazdırma
+            if (harfBulundu)
+            {
+                label1.Text = new string(gizliKelime); 
+            }
+            else
+            {
+                // yanlış harf uyarısı
+                MessageBox.Show("Yanlış tahmin! Tekrar deneyin.");
+            }
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            string kullaniciGirdisi = textBox2.Text;
+            textBox2.Text = (kullaniciGirdisi); //tahmin doğruysa kazandınız, yanlışsa kaybettiniz bildirimi
+
+            if (dogruKelime == kullaniciGirdisi)
+            {
+                textBox3.Text = ("Kelime: " + secilenKelime);
+                textBox4.Text = ("KAZANDINIZ");
+
+            }
+            else
+            {
+                textBox3.Text = ("Kelime: " + secilenKelime);
+                textBox4.Text = ("KAYBETTİNİZ");
+
+            }
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBox1.PasswordChar = '*';
+        }
+
+    
+
+        private void pictureEdit1_EditValueChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void textEdit2_EditValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
